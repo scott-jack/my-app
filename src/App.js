620ede447d935dd6eab43e58159f65b006f58c0f
scott-jack <scottjack2004@gmail.com>
2023-11-28 17:32:21 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Widget from "./Widget";
 
 function App() {
+  // const [clicked, setClicked] = useState(0);
+
+  // const click = () => {
+  //   setClicked(clicked + 1);
+  //   console.log("Yoooo I printed something to the console");
+  // }
+
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  // Update state methods
+  const updateInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  const addTodo = () => {
+    if(input !== "") {
+      const updatedTodo = [...todos, input];
+      setTodos(updatedTodo);
+      setInput("");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div style={{ textAlign: "center" }}>
+    //   <Widget text="Web Impact is awesome!" click={click}/>
+    //   <p>The button was clicked {clicked} times :)</p>
+    // </div>
+    <div className = "App">
+      {/* provide input form */}
+      <label htmlFor="newTodo"> Enter Todo Item: </label>
+      
+      <input type="text" id="newTodo" value={input} onChange={updateInput}></input>
+
+      <button onClick={addTodo}>Enter</button>
     </div>
   );
 }
